afdc5a748ec5d4ddd236d7e3b2529c7dada1134e
Doğukan Arslan <dogukan2arslan@hotmail.com>
2019-11-12 13:37:39 +0300

    ilk commit

--- src/CoinFlipper.js
+++ src/CoinFlipper.js
@@ -9,12 +9,34 @@ class CoinFlipper extends Component {
     // Daha sonra şu anda paranın dönüp dönmeme durumunu da veriyoruz, başlangıçta para atılmamış olduğundan "false" olarak verdik.
     this.state = {
         side: "tura",
-        donuyor: false
+        donuyor: false,
+        flips: 0,
+        yazi: 0,
+        tura: 0
     }
+  }
+
+  sifirla = () => {
+    this.setState({flips: 0});
+    this.setState({yazi: 0});
+    this.setState({tura: 0})
   }
   handleClick = () => {
     // "At!" butonuna tıkladığımızda paranın dönmesini istiyoruz, bu yüzden "donuyor" durumunu "true" yapıyoruz.
     this.setState({donuyor: true});
+    // "At!" butonuna tıklandığında toplam atış sayısını bir arttırıyor
+    this.setState({flips: this.state.flips+1});
+    let flippedCoin = Math.floor(Math.random()*2);
+    if (flippedCoin === 0){
+      this.setState({side: "yazi"})
+    } else if (flippedCoin === 1){
+      this.setState({side: "tura"})
+    }
+    if (flippedCoin === 0){
+      this.setState({yazi: this.state.yazi + 1})
+    } else if(flippedCoin === 1){
+      this.setState({tura: this.state.tura + 1})
+    }
     // 1 saniye kadar dönmesi yeterli, bu yüzden 1 saniye sonra "donuyor" durmunu tekrar "false" yapıyoruz.
     setTimeout(() => this.setState({donuyor: false}), 1000);
   };
@@ -23,16 +45,17 @@ class CoinFlipper extends Component {
     return (
       <div className="CoinFlipper">
         <h1>Yazı mı Tura mı?</h1>
-        <Coin side={this.state.side} donuyor={this.state.donuyor} />
-        <button onClick={this.handleClick} >At!</button>
+        <Coin side={this.state.side} donuyor={this.state.donuyor}/>
+        <button onClick={this.handleClick} >At!</button><br/>
+        <button onClick={this.sifirla}>Sıfırla</button>
         <p>
             Toplam
-            <strong> 5 </strong>
+            <strong> {this.state.flips} </strong>
             atıştan
-            <strong> 3 </strong>
-            ü tura
-            <strong> 2 </strong>
-            si yazı geldi.</p>
+            <strong> {this.state.tura} </strong>
+             tura
+            <strong> {this.state.yazi} </strong>
+             yazı geldi.</p>
       </div>
     )
   }
